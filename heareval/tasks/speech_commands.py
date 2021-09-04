2a84c706e7b9cb44ee31d365b83613e2b1c95fa0
Joseph Turian <turian@gmail.com>
2021-09-04 05:44:48 +0200

    Removed assert

--- heareval/tasks/speech_commands.py
+++ heareval/tasks/speech_commands.py
@@ -141,7 +141,6 @@ class ExtractMetadata(pipeline.ExtractMetadata):
     def speaker_hash(unique_filestem: str) -> str:
         """Get the speaker hash as the Split key for speech_commands"""
         hsh = re.sub(r"_nohash_.*$", "", unique_filestem)
-        assert hsh != unique_filestem, f"{unique_filestem} has no speaker hash."
         return hsh
 
     @staticmethod
