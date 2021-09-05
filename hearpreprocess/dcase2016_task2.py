d4e82d53db3bff22de38527af50a73b190775a11
Joseph Turian <turian@gmail.com>
2021-09-05 22:21:04 +0200

    DCASE: Mix dev + eval

--- hearpreprocess/dcase2016_task2.py
+++ hearpreprocess/dcase2016_task2.py
@@ -4,7 +4,9 @@ Pre-processing pipeline for DCASE 2016 task 2 task (sound event
 detection).
 
 The HEAR 2021 variation of DCASE 2016 Task 2 is that we ignore the
-monophonic training data and use the dev data for train.
+monophonic training data. We mix the dev and eval data, and
+re-partition ourselves, to reduce variance between the partitions.
+
 We also allow training data outside this task.
 """
 
@@ -35,11 +37,13 @@ task_config = {
     "download_urls": [
         {
             "split": "train",
+            "name": "dev",
             "url": "https://archive.org/download/dcase2016_task2_train_dev/dcase2016_task2_train_dev.zip",  # noqa: E501
             "md5": "4e1b5e8887159193e8624dec801eb9e7",
         },
         {
-            "split": "test",
+            "split": "train",
+            "name": "eval",
             "url": "https://archive.org/download/dcase2016_task2_test_public/dcase2016_task2_test_public.zip",  # noqa: E501
             "md5": "ac98768b39a08fc0c6c2ddd15a981dd7",
         },
@@ -48,11 +52,13 @@ task_config = {
         "download_urls": [
             {
                 "split": "train",
+                "name": "dev",
                 "url": "https://github.com/neuralaudio/hear2021-open-tasks-downsampled/raw/main/dcase2016_task2_train_dev-small.zip",  # noqa: E501
                 "md5": "aa9b43c40e9d496163caab83becf972e",
             },
             {
-                "split": "test",
+                "split": "train",
+                "name": "eval",
                 "url": "https://github.com/neuralaudio/hear2021-open-tasks-downsampled/raw/main/dcase2016_task2_test_public-small.zip",  # noqa: E501
                 "md5": "14539d85dec03cb7ac75eb62dd1dd21e",
             },
@@ -63,36 +69,37 @@ task_config = {
 
 
 class ExtractMetadata(pipeline.ExtractMetadata):
-    train = luigi.TaskParameter()
-    test = luigi.TaskParameter()
+    train_dev = luigi.TaskParameter()
+    train_eval = luigi.TaskParameter()
 
     def requires(self):
-        return {"train": self.train, "test": self.test}
+        return {"train_eval": self.train_eval, "train_dev": self.train_dev}
 
     """
     DCASE 2016 uses funny pathing, so we just hardcode the desired
     (paths)
-    Note that for our training data, we only use DCASE 2016 dev data.
-    Their training data is short monophonic events.
     """
-    split_to_path_str = {
-        "train": "dcase2016_task2_train_dev/dcase2016_task2_dev/",
-        "test": "dcase2016_task2_test_public/",
+    requires_key_to_path_str = {
+        "train_dev": "train/dev/dcase2016_task2_train_dev/dcase2016_task2_dev/",
+        "train_eval": "train/eval/dcase2016_task2_test_public/",
     }
 
     def get_requires_metadata(self, split: str) -> pd.DataFrame:
         logger.info(f"Preparing metadata for {split}")
 
-        split_path = (
-            Path(self.requires()[split].workdir)
-            .joinpath(split)
-            .joinpath(self.split_to_path_str[split])
+        assert requires_key.startswith("train_")
+
+        requires_path = Path(self.requires()[requires_key].workdir).joinpath(
+            self.requires_key_to_path_str[requires_key]
         )
 
         metadatas = []
-        for annotation_file in split_path.glob("annotation/*.txt"):
+        for annotation_file in requires_path.glob("annotation/*.txt"):
             metadata = pd.read_csv(
-                annotation_file, sep="\t", header=None, names=["start", "end", "label"]
+                annotation_file,
+                sep="\t",
+                header=None,
+                names=["start", "end", "label"],
             )
             # Convert start and end times to milliseconds
             metadata["start"] *= 1000
@@ -104,74 +111,12 @@ class ExtractMetadata(pipeline.ExtractMetadata):
             )
             metadata = metadata.assign(
                 relpath=sound_file,
-                split=lambda df: split,
+                split=lambda df: "train",
             )
 
             metadatas.append(metadata)
 
         return pd.concat(metadatas).reset_index(drop=True)
-
-    def split_train_test_val(self, metadata: pd.DataFrame) -> pd.DataFrame:
-        """
-        Because the training set is so small, manually partition it into a
-        50/50 dev set that captures all hyperparameters in train and test.
-        """
-
-        splits_present = metadata["split"].unique()
-        assert set(splits_present) == {"train", "test"}
-
-        # Manually split into train + dev to try to balance the hyperparams
-        train_stems = {
-            "dev_1_ebr_-6_nec_1_poly_0",
-            "dev_1_ebr_-6_nec_3_poly_0",
-            "dev_1_ebr_-6_nec_4_poly_1",
-            "dev_1_ebr_6_nec_2_poly_0",
-            "dev_1_ebr_6_nec_3_poly_1",
-            "dev_1_ebr_6_nec_5_poly_1",
-            "dev_1_ebr_0_nec_2_poly_0",
-            "dev_1_ebr_0_nec_3_poly_1",
-            "dev_1_ebr_0_nec_4_poly_1",
-        }
-
-        valid_stems = {
-            "dev_1_ebr_-6_nec_2_poly_0",
-            "dev_1_ebr_-6_nec_3_poly_1",
-            "dev_1_ebr_-6_nec_5_poly_1",
-            "dev_1_ebr_6_nec_1_poly_0",
-            "dev_1_ebr_6_nec_3_poly_0",
-            "dev_1_ebr_6_nec_4_poly_1",
-            "dev_1_ebr_0_nec_1_poly_0",
-            "dev_1_ebr_0_nec_3_poly_0",
-            "dev_1_ebr_0_nec_5_poly_1",
-        }
-        assert len(train_stems) + len(valid_stems) == len(train_stems | valid_stems)
-
-        # Gross, let's never do this again
-        if self.task_config["version"].split("-")[-1] == "small":
-            assert train_stems | valid_stems >= set(
-                metadata[metadata.split == "train"]["unique_filestem"].unique()
-            )
-        else:
-            assert train_stems | valid_stems == set(
-                metadata[metadata.split == "train"]["unique_filestem"].unique()
-            )
-        metadata.reset_index(drop=True, inplace=True)
-        metadata.loc[metadata["unique_filestem"].isin(valid_stems), "split"] = "valid"
-        if self.task_config["version"].split("-")[-1] == "small":
-            assert train_stems >= set(
-                metadata[metadata.split == "train"]["unique_filestem"].unique()
-            )
-            assert valid_stems >= set(
-                metadata[metadata.split == "valid"]["unique_filestem"].unique()
-            )
-        else:
-            assert train_stems == set(
-                metadata[metadata.split == "train"]["unique_filestem"].unique()
-            )
-            assert valid_stems == set(
-                metadata[metadata.split == "valid"]["unique_filestem"].unique()
-            )
-        return metadata
 
 
 def main(
