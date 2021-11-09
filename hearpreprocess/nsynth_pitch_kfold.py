0bcecc6f4a0bfc0ff8b2d2d9de44e977bb577a78
Joseph Turian <turian@gmail.com>
2021-11-10 00:39:35 +0100

    Small fixes

--- hearpreprocess/nsynth_pitch_kfold.py
+++ hearpreprocess/nsynth_pitch_kfold.py
@@ -3,16 +3,16 @@
 Pre-processing pipeline for NSynth pitch detection
 """
 
-from nsynth_pitch import *
+from hearpreprocess.nsynth_pitch import *
 
 generic_task_config["split_mode"] = "new_split_kfold"
 generic_task_config["nfolds"] = 5
 
-for mode in generic_task_config["modes"]:
+for mode in list(generic_task_config["modes"].keys()):
     generic_task_config["modes"]["max_task_duration_by_fold"] = generic_task_config[
         "modes"
-    ]["max_task_duration_by_split"]["test"]
-    del generic_task_config["modes"]["max_task_duration_by_split"]
+    ][mode]["max_task_duration_by_split"]["test"]
+    del generic_task_config["modes"][mode]["max_task_duration_by_split"]
 
 
 # This works on train/val/test, which for kfold might b0rk.
