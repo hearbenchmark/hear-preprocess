682f0902b3614f289c2babd6cd028acb8846485f
Joseph Turian <turian@gmail.com>
2021-11-10 00:59:18 +0100

    Comment

--- hearpreprocess/nsynth_pitch_kfold.py
+++ hearpreprocess/nsynth_pitch_kfold.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 """
-Pre-processing pipeline for NSynth pitch detection
+Pre-processing pipeline for NSynth pitch detection, but with 10-fold
+as a way of checking kfold versus 80/10/10 train/val/test split.
 """
 
 from hearpreprocess.nsynth_pitch import *
 
 generic_task_config["task_name"] = "nsynth_pitch_kfold"
 generic_task_config["split_mode"] = "new_split_kfold"
-generic_task_config["nfolds"] = 5
+# train/val/test is 80/10/10, so 10-fold is identical in split proportions
+generic_task_config["nfolds"] = 10
 
+# generic_task_config["modes"]["5h"] = {"max_task_duration_by_fold": 3600 * 5 * TEST_PERCENTAGE / TRAINVAL_PERCENTAGE}
+# generic_task_config["modes"]["50h"] = {"max_task_duration_by_fold": 36000 * 5 * TEST_PERCENTAGE / TRAINVAL_PERCENTAGE}
+# generic_task_config["modes"]["small"]["max_task_duration_by_fold"] = None
+# del generic_task_config["modes"]["small"]["max_task_duration_by_split"]
+
+# Keep the test length (same as validation) for every fold
 for mode in list(generic_task_config["modes"].keys()):
     generic_task_config["modes"][mode][
         "max_task_duration_by_fold"
